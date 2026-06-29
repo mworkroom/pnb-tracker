041d4bfff6e32aa6acf75c934b6a57c5d05baaf5
mworkroom <easylong@gmail.com>
2026-06-30 05:50:17 +0900

    Add files via upload

--- supabase.config.js
+++ supabase.config.js
@@ -1,3 +1,3 @@
-export const SUPABASE_URL = "https://ddlwainwollvpaeccpty.supabase.co";
-export const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_PJkTB8OFlF2_fgTqSkrKlw_rRk5VUVN";
+export const SUPABASE_URL = "https://jnyfiggkmbjuiebvbsdk.supabase.co";
+export const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_eOBdwbvPo6T3xPV-8fg-mQ_KqpAfs_Y";
 
